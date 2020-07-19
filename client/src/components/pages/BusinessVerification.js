357a8c6fd2697b03913940dbe00b2269acff69aa
Caroline Ninganga <carolineninganga@Carolines-MBP.home>
2020-07-19 14:40:14 -0400

    some changes

---
+++ client/src/components/pages/BusinessVerification.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import ImageUploader from 'react-images-upload';
+ 
+class BusinessVerification extends Component {
+ 
+    constructor(props) {
+        super(props);
+         this.state = { pictures: [], showSubmitButton: false };
+         this.onDrop = this.onDrop.bind(this);
+    }
+ 
+    onDrop(picture) {
+        console.log(picture)
+        if(picture.length === 0){
+            this.setState({
+                showSubmitButton: false
+            })
+        } else {
+            this.setState({ 
+                showSubmitButton: true 
+            })
+        }
+
+    
+        this.setState({
+            pictures: this.state.pictures.concat(picture),
+        });
+    }
+ 
+    render() {
+        return (
+            <div className="container" style={{ width:'75%', marginTop:'20%'}}>
+            <h3 className="title">Please upload your business verification certificate</h3>
+            <ImageUploader
+                withIcon={true}
+                buttonText='Choose images'
+                onChange={this.onDrop}
+                imgExtension={['.jpg', '.gif', '.png', '.gif']}
+                maxFileSize={5242880}
+                withPreview={true}
+            />
+            {this.state.showSubmitButton ? <button id="btn1" className='btn btn-primary' onClick={()=>{this.props.history.push('/businessProfile'); alert("Your certificate has been uploaded successfull")}}>submit</button> : ""} 
+            </div>
+        );
+    }
+}
+
+export default BusinessVerification;
